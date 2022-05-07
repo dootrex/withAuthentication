270c82c1382248fe31c66d78acf8c4af6a5750e9
patthekid99 <pattyvan4@gmail.com>
2022-05-07 15:18:08 -0700

    added company name to realtor return and routes to filter by company and rating

--- withAuthentication/Controllers/DirectoryController.cs
+++ withAuthentication/Controllers/DirectoryController.cs
@@ -23,7 +23,7 @@ namespace withAuthentication.Controllers
         public IActionResult getAllReators()
         {
 
-            return Ok(_context.Realtors.Select(r => new { firstName = r.FirstName, lastName = r.LastName, realtorID = r.RealtorId, profilePic = r.ProfilePic }));
+            return Ok(_context.Realtors.Select(r => new { firstName = r.FirstName, lastName = r.LastName, realtorID = r.RealtorId, companyName = r.CompanyName, profilePic = r.ProfilePic }));
         }
 
 
@@ -66,14 +66,51 @@ namespace withAuthentication.Controllers
         public IActionResult getRealtorByName(string query)
         {
 
-            return Ok(_context.Realtors.Where(r => (r.FirstName + " " + r.LastName).Contains(query)).Select(r => new { firstName = r.FirstName, lastName = r.LastName, realtorID = r.RealtorId, profilePic = r.ProfilePic }));
+            return Ok(_context.Realtors.Where(r => (r.FirstName + r.LastName).Contains(query)).Select(r => new { firstName = r.FirstName, lastName = r.LastName, realtorID = r.RealtorId, companyName = r.CompanyName, profilePic = r.ProfilePic }));
         }
         [HttpGet]
         [Route("realtors/lang/{langID}")]
         public IActionResult getRealtorsByLanguage(long langID)
         {
-            var realtors = _context.RealtorLanguages.Where(rl => rl.LanguageId == langID).Select(rl => new { firstName = rl.Realtor.FirstName, lastName = rl.Realtor.LastName, profilePic = rl.Realtor.ProfilePic, realtorID = rl.Realtor.RealtorId });
+            var realtors = _context.RealtorLanguages.Where(rl => rl.LanguageId == langID).Select(rl => new { firstName = rl.Realtor.FirstName, lastName = rl.Realtor.LastName, profilePic = rl.Realtor.ProfilePic, companyName = rl.Realtor.CompanyName, realtorID = rl.Realtor.RealtorId });
             return Ok(realtors);
+        }
+
+        [HttpGet]
+        [Route("realtors/company/{query}")]
+        public IActionResult getRealtorByCompany(string query) //needs to be edited somehow to get companies with spaces in names
+        {
+            return Ok(_context.Realtors.Where(r => (r.CompanyName).Contains(query)).Select(r => new { firstName = r.FirstName, lastName = r.LastName, realtorID = r.RealtorId, companyName = r.CompanyName, profilePic = r.ProfilePic }));
+        }
+
+        [HttpGet]
+        [Route("realtors/rating/{value}")]
+        public IActionResult getRealtorByRating(string value)
+        {
+            if (value == "ascending")
+            {
+                return Ok(_context.Realtors.OrderBy(r => r.AvgStarRating).Select(r => new { firstName = r.FirstName, lastName = r.LastName, realtorID = r.RealtorId, companyName = r.CompanyName, profilePic = r.ProfilePic }));
+            }
+            else
+            {
+                return Ok(_context.Realtors.OrderByDescending(r => r.AvgStarRating).Select(r => new { firstName = r.FirstName, lastName = r.LastName, realtorID = r.RealtorId, companyName = r.CompanyName, profilePic = r.ProfilePic }));
+            }
+
+        }
+
+        [HttpGet]
+        [Route("developers/rating/{value}")]
+        public IActionResult getDeveloperByRating(string value)
+        {
+            if (value == "ascending")
+            {
+                return Ok(_context.Developers.OrderBy(d => d.AvgStarRating).Select(d => new { developerID = d.DeveloperId, developerName = d.DeveloperName, logo = d.Logo }));
+            }
+            else
+            {
+                return Ok(_context.Developers.OrderByDescending(d => d.AvgStarRating).Select(d => new { developerID = d.DeveloperId, developerName = d.DeveloperName, logo = d.Logo }));
+            }
+
         }
     }
 }
